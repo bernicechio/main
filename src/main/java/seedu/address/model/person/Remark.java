97b5435b72b3d12037bf1ec594844de74b0c5a7b
Bernice Chio <bernicechiohuiyin@gmail.com>
2019-09-22 23:33:47 +0800

    Add new Remark command

--- src/main/java/seedu/address/model/person/Remark.java
+++ src/main/java/seedu/address/model/person/Remark.java
@@ -1,40 +1,17 @@
 package seedu.address.model.person;
 
 import static java.util.Objects.requireNonNull;
-import static seedu.address.commons.util.AppUtil.checkArgument;
 
 /**
- * Represents a Person's address in the address book.
- * Guarantees: immutable; is valid as declared in {@link #isValidAddress(String)}
+ * Represents a Person's remark in the address book.
+ * Guarantees: immutable; is always valid
  */
 public class Remark {
-
-    public static final String MESSAGE_CONSTRAINTS = "Addresses can take any values, and it should not be blank";
-
-    /*
-     * The first character of the address must not be a whitespace,
-     * otherwise " " (a blank string) becomes a valid input.
-     */
-    public static final String VALIDATION_REGEX = "[^\\s].*";
-
     public final String value;
 
-    /**
-     * Constructs an {@code Address}.
-     *
-     * @param address A valid address.
-     */
-    public Remark(String address) {
-        requireNonNull(address);
-        checkArgument(isValidAddress(address), MESSAGE_CONSTRAINTS);
-        value = address;
-    }
-
-    /**
-     * Returns true if a given string is a valid email.
-     */
-    public static boolean isValidAddress(String test) {
-        return test.matches(VALIDATION_REGEX);
+    public Remark(String remark) {
+        requireNonNull(remark);
+        value = remark;
     }
 
     @Override
@@ -53,5 +30,4 @@ public class Remark {
     public int hashCode() {
         return value.hashCode();
     }
-
 }
